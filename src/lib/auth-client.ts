3f35b1011f31107957cae10ee8f2ffd6375207aa
Vardan Aggarwal <vardan.netweb@gmail.com>
2025-11-04 15:46:00 +0530

    url change

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://project-avoj9r87o-vardans-projects-378735dd.vercel.app/"
+    baseURL: process.env.NEXT_PUBLIC_API_URL
 })
 
